9e0d53127af900934638c8d5fa8cdac7f2765ba5
Chiamaka Ofonagoro <chiamakaofo@gmail.com>
2025-06-11 02:32:48 -0400

    built activity classifier with naive bayes

--- backend/text_classify.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("wldnjs057/diary_entry")
-print(ds['test']['diary_entry'][:5])
-
-for line in ds['test']['diary_entry']:
-    print(f"\n{line}")
